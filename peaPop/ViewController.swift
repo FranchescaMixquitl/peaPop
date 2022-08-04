cf0519db90d163832e266e540e88fce8a3b44264
Franchesca <franchescamixquitl@gmail.com>
2022-08-04 10:20:55 -0700

    initial

--- peaPop/ViewController.swift
+++ peaPop/ViewController.swift
@@ -6,14 +6,82 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    let popSound = URL(fileURLWithPath: Bundle.main.path(forResource: "pop-39222", ofType: "mp3")!)
+    var audioPlayer = AVAudioPlayer()
+    
+    
+    var buttonClickerOne = 1
+    var buttonClickedTwo = 1
+    
+    
+    @IBOutlet var peaOne: UIImageView!
+    
+    
+    @IBOutlet var peaTwo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func buttonClicked2(_ sender: Any) {
+        if buttonClickedTwo%2 != 0{
+            buttonClickedTwo = 2
+            let imgPeaOne: UIImage = UIImage(named: "pea")!
+            peaTwo.image = imgPeaOne
+            do {
+                audioPlayer = try AVAudioPlayer(contentsOf: popSound)
+                audioPlayer.play()
+            } catch {
+                print("catch error")
+            }
+        }
+        else{
+            buttonClickedTwo = 1
+            let imgCloudOne: UIImage = UIImage(named: "cloud")!
+            peaTwo.image = imgCloudOne
+            do {
+                audioPlayer = try AVAudioPlayer(contentsOf: popSound)
+                audioPlayer.play()
+            } catch {
+                print("catch error")
+            }
+            
+        }
 
+        
+    }
+    @IBAction func buttonClicked(_ sender: Any) {
+
+        if buttonClickerOne%2 != 0{
+            buttonClickerOne = 2
+            let imgPeaOne: UIImage = UIImage(named: "pea")!
+            peaOne.image = imgPeaOne
+            do {
+                audioPlayer = try AVAudioPlayer(contentsOf: popSound)
+                audioPlayer.play()
+            } catch {
+                print("catch error")
+            }
+        }
+        else{
+            buttonClickerOne = 1
+            let imgCloudOne: UIImage = UIImage(named: "cloud")!
+            peaOne.image = imgCloudOne
+            do {
+                audioPlayer = try AVAudioPlayer(contentsOf: popSound)
+                audioPlayer.play()
+            } catch {
+                print("catch error")
+            }
+        }
+
+        
+    }
+    
 }
 
